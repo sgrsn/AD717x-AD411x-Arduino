703b29bdf81a7ba19c8580727d3cc206090e3f09
main <sgrsn1711@gmail.com>
2023-09-29 18:40:38 +0900

    Fix SPIClass and CS to be selectable

--- include/platform_drivers.h
+++ include/platform_drivers.h
@@ -4,12 +4,19 @@
 
 #define SPI_DELAY_MICROSECONDS 50
 
-typedef SPIClass spi_desc;
+typedef struct{
+  SPIClass* spi;
+  uint8_t cs_pin;
+} spi_with_cs;
+
+typedef spi_with_cs spi_desc;
 
 typedef struct{
+  SPIClass* spi = &SPI;
+  uint8_t cs_pin;
   uint8_t spi_mode = SPI_MODE3;
 } spi_init_param;
 
-int32_t spi_init(spi_desc** spi_desc, spi_init_param* spi_init);
+int32_t spi_init(spi_desc** pp_spi_desc, spi_init_param* spi_init);
 int32_t spi_write_and_read(spi_desc* spi_desc, uint8_t* wrBuf, uint8_t len);
 int32_t spi_remove(spi_desc* spi_desc);
--- src/platform_drivers.cpp
+++ src/platform_drivers.cpp
@@ -1,26 +1,28 @@
 #include "platform_drivers.h"
 
 /*Arduino imlementation*/
-int32_t spi_init(spi_desc** spi_desc, spi_init_param* spi_init)
+int32_t spi_init(spi_desc** pp_spi_desc, spi_init_param* spi_init)
 {
+	(*pp_spi_desc) = (spi_desc *)malloc(sizeof(**pp_spi_desc));
   /* initiate SPI communication */
-	(*spi_desc) = &SPI;
-	(*spi_desc)->begin();
-	(*spi_desc)->setDataMode(spi_init->spi_mode);
-	pinMode(SS, OUTPUT);
+	(*pp_spi_desc)->spi = spi_init->spi;
+	(*pp_spi_desc)->cs_pin = spi_init->cs_pin;
+	(*pp_spi_desc)->spi->begin();
+	(*pp_spi_desc)->spi->setDataMode(spi_init->spi_mode);
+	pinMode((*pp_spi_desc)->cs_pin, OUTPUT);
 	return 0;
 }
 
 int32_t spi_write_and_read(spi_desc* spi_desc, uint8_t* wrBuf, uint8_t len)
 {	
 	/* toggle the chip select */
-	digitalWrite(SS, HIGH);
+	digitalWrite(spi_desc->cs_pin, HIGH);
 	delayMicroseconds(SPI_DELAY_MICROSECONDS);
-	digitalWrite(SS, LOW);
+	digitalWrite(spi_desc->cs_pin, LOW);
 	/* allow the LDOs to power up */
   for(int i = 0; i < len; i++)
   {
-    uint8_t readBuf = spi_desc->transfer(wrBuf[i]); 
+    uint8_t readBuf = spi_desc->spi->transfer(wrBuf[i]); 
     wrBuf[i] = readBuf;
   } 
 	delayMicroseconds(SPI_DELAY_MICROSECONDS);
@@ -29,5 +31,5 @@ int32_t spi_write_and_read(spi_desc* spi_desc, uint8_t* wrBuf, uint8_t len)
 
 int32_t spi_remove(spi_desc* spi_desc)
 {
-	spi_desc->end();
+	spi_desc->spi->end();
 }
