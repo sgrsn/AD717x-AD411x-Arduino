75fc58b381c53d4f5a597cb8db82b7b62eada4f1
main <sgrsn1711@gmail.com>
2023-12-09 23:06:51 +0900

    Add example implementation to add read channel

--- examples/ad7177_main.cpp
+++ examples/ad7177_main.cpp
@@ -15,7 +15,9 @@ void setup() {
 
   std::stringstream ss;
   
-  ad717x_st_reg reg[10] = {};
+  const size_t reg_size = 15;
+  ad717x_st_reg reg[reg_size] = {};
+
   reg[0].addr = AD717X_ADCMODE_REG;
   reg[0].value = AD717X_ADCMODE_REG_REF_EN | AD717X_ADCMODE_REG_MODE(0) | AD717X_ADCMODE_REG_CLKSEL(0);
   reg[0].size = 2;
@@ -35,34 +37,54 @@ void setup() {
   reg[3].value = 0x800000;
   reg[3].size = 3;
 
-  /*ID register; returns 16-bit model-specific ID; unique value for AD7177-2 is 0x4FDX*/
-  reg[4].addr = AD717X_ID_REG;
-  reg[4].value = 0x0000;
-  reg[4].size = 2;
+  reg[4].addr = AD717X_OFFSET1_REG;
+  reg[4].value = 0x800000;
+  reg[4].size = 3;
 
-  /*Enable CH0*/
-  reg[5].addr = AD717X_CHMAP0_REG;
-  reg[5].value = AD717X_CHMAP_REG_CH_EN | AD717X_CHMAP_REG_SETUP_SEL(0) | AD717X_CHMAP_REG_AINPOS(0) | AD717X_CHMAP_REG_AINNEG(1);
+  /*ID register; returns 16-bit model-specific ID; unique value for AD7177-2 is 0x4FDX*/
+  reg[5].addr = AD717X_ID_REG;
+  reg[5].value = 0x0000;
   reg[5].size = 2;
 
-  /*Set as differential input with AIN0 positive input and AIN1 negative input*/
-  reg[6].addr = AD717X_SETUPCON0_REG;
-  reg[6].value = AD717X_SETUP_CONF_REG_BI_UNIPOLAR | AD717X_SETUP_CONF_REG_AINBUF_P | AD717X_SETUP_CONF_REG_AINBUF_N | AD717X_SETUP_CONF_REG_REF_SEL(0b00);
+  /*Enable CH0*/
+  reg[6].addr = AD717X_CHMAP0_REG;
+  reg[6].value = AD717X_CHMAP_REG_CH_EN | AD717X_CHMAP_REG_SETUP_SEL(0) | AD717X_CHMAP_REG_AINPOS(0) | AD717X_CHMAP_REG_AINNEG(1);
   reg[6].size = 2;
-
-  /*Digital filter settings*/
-  reg[7].addr = AD717X_FILTCON0_REG;
-  reg[7].value = AD717X_FILT_CONF_REG_ENHFILTEN | AD717X_FILT_CONF_REG_ENHFILT(0b010) | AD717X_FILT_CONF_REG_ORDER(0) | AD717X_FILT_CONF_REG_ODR(0b00111);
+  /*Enable CH1*/
+  reg[7].addr = AD717X_CHMAP1_REG;
+  reg[7].value = AD717X_CHMAP_REG_CH_EN | AD717X_CHMAP_REG_SETUP_SEL(0) | AD717X_CHMAP_REG_AINPOS(2) | AD717X_CHMAP_REG_AINNEG(3);
   reg[7].size = 2;
 
+  /*Set as differential input with AIN0 positive input and AIN1 negative input*/
+  reg[8].addr = AD717X_SETUPCON0_REG;
+  reg[8].value = AD717X_SETUP_CONF_REG_BI_UNIPOLAR | AD717X_SETUP_CONF_REG_AINBUF_P | AD717X_SETUP_CONF_REG_AINBUF_N | AD717X_SETUP_CONF_REG_REF_SEL(0b00);
+  reg[8].size = 2;
+  
+  reg[9].addr = AD717X_SETUPCON1_REG;
+  reg[9].value = AD717X_SETUP_CONF_REG_BI_UNIPOLAR | AD717X_SETUP_CONF_REG_AINBUF_P | AD717X_SETUP_CONF_REG_AINBUF_N | AD717X_SETUP_CONF_REG_REF_SEL(0b00);
+  reg[9].size = 2;
+
+  /*Digital filter settings*/
+  reg[10].addr = AD717X_FILTCON0_REG;
+  reg[10].value = AD717X_FILT_CONF_REG_ENHFILTEN | AD717X_FILT_CONF_REG_ENHFILT(0b010) | AD717X_FILT_CONF_REG_ORDER(0) | AD717X_FILT_CONF_REG_ODR(0b00111);
+  reg[10].size = 2;
+
+  reg[11].addr = AD717X_FILTCON1_REG;
+  reg[11].value = AD717X_FILT_CONF_REG_ENHFILTEN | AD717X_FILT_CONF_REG_ENHFILT(0b010) | AD717X_FILT_CONF_REG_ORDER(0) | AD717X_FILT_CONF_REG_ODR(0b00111);
+  reg[11].size = 2;
+
   /*Data register, where ADC conversion results are stored.*/
-  reg[8].addr = AD717X_DATA_REG;
-  reg[8].value = 0;
-  reg[8].size = 4;
+  reg[12].addr = AD717X_DATA_REG;
+  reg[12].value = 0;
+  reg[12].size = 4;
+
+  reg[13].addr = AD717X_STATUS_REG;
+  reg[13].value = 0;
+  reg[13].size = 1;
 
   ad717x_dev device;
   device.regs = reg;
-  device.num_regs = 10;
+  device.num_regs = reg_size;
   device.useCRC = AD717X_DISABLE;
 
   ad717x_dev *pdevice = &device;
@@ -72,7 +94,7 @@ void setup() {
   init_param.spi_init.cs_pin = SS;
   init_param.spi_init.spi_mode = SPI_MODE3;
   init_param.regs = reg;
-  init_param.num_regs = 10;
+  init_param.num_regs = reg_size;
 
   AD717X_Init(&pdevice, init_param);
 
@@ -82,42 +104,40 @@ void setup() {
   Serial.println(ss.str().c_str());
   ss.str("");
 
-  AD717X_WriteRegister(pdevice, AD717X_CHMAP0_REG);  
-  AD717X_ReadRegister(pdevice, AD717X_CHMAP0_REG);
-  ss << "[" << std::hex << AD717X_CHMAP0_REG << "] : " << AD717X_GetReg(pdevice, AD717X_CHMAP0_REG)->value;
-  Serial.println(ss.str().c_str());
-  ss.str("");
+  AD717X_WriteRegister(pdevice, AD717X_CHMAP0_REG);
+  AD717X_WriteRegister(pdevice, AD717X_CHMAP1_REG);
+  AD717X_WriteRegister(pdevice, AD717X_SETUPCON0_REG);
+  AD717X_WriteRegister(pdevice, AD717X_SETUPCON1_REG);
+  AD717X_WriteRegister(pdevice, AD717X_FILTCON0_REG);
+  AD717X_WriteRegister(pdevice, AD717X_FILTCON1_REG);
 
-  AD717X_WriteRegister(pdevice, AD717X_SETUPCON0_REG);  
-  AD717X_ReadRegister(pdevice, AD717X_SETUPCON0_REG);
-  ss << "[" << std::hex << AD717X_SETUPCON0_REG << "] : " << AD717X_GetReg(pdevice, AD717X_SETUPCON0_REG)->value;
-  Serial.println(ss.str().c_str());
-  ss.str("");
-
-  AD717X_WriteRegister(pdevice, AD717X_FILTCON0_REG);  
-  AD717X_ReadRegister(pdevice, AD717X_FILTCON0_REG);
-  ss << "[" << std::hex << AD717X_FILTCON0_REG << "] : " << AD717X_GetReg(pdevice, AD717X_FILTCON0_REG)->value;
-  Serial.println(ss.str().c_str());
-  ss.str("");
+  const double Gain = 0x555555;
+  const double Vref = 5.0;
+  const double offset = 0x800000;
+  double V0 = 0;
+  double V1 = 0;
 
   int32_t data = 0;
   while(1)
   {
     AD717X_ReadData(pdevice, &data);
-    double Gain = 0x555555;
-    double Vref = 5.0;
-    double offset = 0x800000;
+    AD717X_ReadRegister(pdevice, AD717X_STATUS_REG);
 
-    // Data Conversion Formula
-    /*
-      DATA = ( (0.75 * VIN / REF) * 2^23 - ( Offset - 0x800000 ) ) * Gain / 0x400000 * 2
+    uint32_t ch = (AD717X_GetReg(pdevice, AD717X_STATUS_REG)->value) & (uint32_t)0b11;
+
+    /* Data Conversion Formula
+     DATA = ( (0.75 * VIN / REF) * 2^23 - ( Offset - 0x800000 ) ) * Gain / 0x400000 * 2
     */
+    if(ch == 0)
+      V0 = ((data/2/Gain) * 0x400000 + (offset-(double)0x800000)) / 0x800000 / 0.75 * Vref;
+    else if(ch == 1)
+      V1 = ((data/2/Gain) * 0x400000 + (offset-(double)0x800000)) / 0x800000 / 0.75 * Vref;
 
-    double Vin = ((data/2/Gain) * 0x400000 + (offset-(double)0x800000)) / 0x800000 / 0.75 * Vref;
-    Serial.print("Voltage (AIN0 - AIN1) : ");
-    Serial.print(Vin, 3);
-    Serial.println(" [V]");
-    delay(100);
+    ss.str("");
+    ss << "\033[37m" << "CH0 (AIN0 - AIN1) : " << "\033[33m" << V0 << " [V]";
+    ss << "\033[37m" << ", CH1 (AIN2 - AIN3) : " << "\033[33m" <<  V1 << " [V]";;
+    Serial.println(ss.str().c_str());
+    delay(1);
   }
 }
 
